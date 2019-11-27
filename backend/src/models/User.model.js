591a6952ee3d82cc7b9d77118cf4aa0e53929bb9
Giovanni Gómez Restrepo <giovagr20@users.noreply.github.com>
2019-11-26 20:41:09 -0500

    Created all API for login in Angular

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -3,6 +3,8 @@ const {Schema, model} = require('mongoose');
 const _userSchema = new Schema({
     email: String,
     password: String
+},{
+    timestamps: true
 });
 
 module.exports = model('User',  _userSchema);
--- backend/src/routes/routes.index.js
+++ backend/src/routes/routes.index.js
@@ -1,14 +1,95 @@
-const {Router} = require('express');
+const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const router = Router();
 
 const _user = require('../models/User.model');
 
 
-router.get('/', (req, res)=> res.send('<h1>Hello world</h1>'));
+router.get('/', (req, res) => res.send('<h1>Hello world</h1>'));
 
-router.post('/signup', (req,res)=>{
-    console.log(req.body);
-    res.send('Register');
-})  
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+
+    const newUser = new _user({ email, password });
+    await newUser.save();
+
+    const token = jwt.sign({ _id: newUser._id },
+        'secretKey');
+    res.status(200).json(token);
+
+});
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await _user.findOne({ email });
+    if (!user) return res.status(401).send("The email doesn't exist");
+    if (user.password !== password) return res.status(401).send('Wrong password');
+
+    const token = jwt.sign({ _id: user._id }, 'secretKey');
+    res.status(200).json(token);
+
+});
+
+router.get('/task', async (req, res) => {
+    res.json([
+        {
+            _id: 1,
+            name: 'Giovanni',
+            description: 'Lorem ipsum daf retop',
+            date: '2019-11-25'
+        },
+        {
+            _id: 2,
+            name: 'Isabel',
+            description: 'Lorem ipsum daf retop',
+            date: '2019-11-25'
+        },
+        {
+            _id: 3,
+            name: 'Andrea',
+            description: 'Lorem ipsum daf retop',
+            date: '2019-11-25'
+        }]);
+});
+
+router.get('/private-tasks', verifyToken,(req, res) => {
+    res.json([{
+        _id: 1,
+        name: 'Giovanni',
+        description: 'Lorem ipsum daf retop',
+        date: '2019-11-25'
+    },
+    {
+        _id: 2,
+        name: 'Isabel',
+        description: 'Lorem ipsum daf retop',
+        date: '2019-11-25'
+    },
+    {
+        _id: 3,
+        name: 'Andrea',
+        description: 'Lorem ipsum daf retop',
+        date: '2019-11-25'
+    }]);
+});
+
+router.get('/profile', verifyToken, (req,res)=>{
+    res.send(req.userId);
+});
 
 module.exports = router;
+
+function verifyToken(req,res,next){
+ if(!req.headers.authorization) return res.status(401).send('Unauthorized request');
+
+ const token=req.headers.authorization.split(' ')[1]
+
+ if(token==='null'){
+     return res.status(401).send('Unauthorized request');
+ }
+
+ const payload = jwt.verify(token, 'secretKey');
+ req.userId = payload._id;
+ next();
+}
